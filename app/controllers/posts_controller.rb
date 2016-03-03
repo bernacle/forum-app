ce84b52d57e78344923eaa6a8a3cdb5a6696387f
Bruno Simplicio <brunoraphael89@gmail.com>
2016-03-03 20:10:11 -0300

    Add post model & controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+end
